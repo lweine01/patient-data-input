771dc9ed4b9c6d074483407cb9a696b1c1ee1140
lweine01 <laurarweiner@gmail.com>
2021-10-31 22:06:46 -0400

    updated file structure, created static html page and express routes.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const htmlRoute = require('./routes/html');
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(htmlRoute);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
